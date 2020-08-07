ab2e03b7c4dcd36207931ab509a9460bef360850
Pranit Patil <65124146+pranithcrk@users.noreply.github.com>
2020-08-07 22:51:43 +0530

    Create SimpleInterestCalculator.py

---
+++ MiscCodes/SimpleInterestCalculator.py
@@ -0,0 +1,6 @@
+P=float(input('Enter the Principle amount : '))
+N=float(input('Enter the number of years : '))
+R=float(input('Rate of Interest being offered : '))
+
+SI=(P*R*N)/100
+print('interest earned = {}'.format(SI))
